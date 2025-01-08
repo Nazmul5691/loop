8cbd0d3d2bf1e8c2801d32ad05631f9959fd0f51
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-08 15:29:13 +0600

    added sum in while loop

--- while-loop.js
+++ while-loop.js
@@ -1,5 +1,28 @@
-let num = 0;
-while( num < 7){
-    console.log('i love you');
-    num = num+1;
+// let num = 0;
+// while( num < 7){
+//     console.log('i love you');
+//     num = num+1;
+// }
+
+
+// let num = 1;
+// sum = 0;
+
+// while ( num <= 10 ){
+//     console.log(num);
+//     sum = sum + num;
+//     console.log(sum);
+//     num++;
+// }
+
+
+let num = 1;
+
+
+while ( num <= 10 ){
+    // console.log(num);
+    if(num % 2 == 0){
+        console.log(num);
+    }
+    num++;
 }
