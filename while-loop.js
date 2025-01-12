50843ea211ac4b6fca22171d22e07f3bc1231708
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-01-12 22:55:50 +0600

    add even

--- while-loop.js
+++ while-loop.js
@@ -21,9 +21,10 @@ let num = 1;
 
 while ( num <= 10 ){
     // console.log(num);
-    
+
     if(num % 2 == 0){
         console.log(num);
+        console.log("Print even number");
     }
     num++;
 }
